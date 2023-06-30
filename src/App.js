5440b5b58dcfef8534a283b869c5b879b36a817d
Dakshk1001 <kdaksh3410@gmail.com>
2023-06-30 14:07:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import "./App.css";
+import Footer from "./myComponents/Footer";
+import Header from "./myComponents/Header";
+import Hero from "./myComponents/Hero";
+import Products from "./myComponents/Products";
+import Slider from "./myComponents/Slider";
+import Testimonials from "./myComponents/Testimonials";
+import Virtual from "./myComponents/Virtual";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
+
+
+    </>
+
+
+  );
+}
+
+export default App;
